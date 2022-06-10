9fc425c4748508e803fedf195479ed5ab298aba4
Tiago Cabral <tiagocabraldafonseca@gmail.com>
2022-06-09 22:32:18 -0300

    Auth and Provider

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,15 @@
-function Button () {
+import {ButtonHTMLAttributes} from "react";
+
+import '../styles/button.scss'
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>
+
+export function Button (props: ButtonProps) {
     return(
-        <button></button>
+        <button
+        className="button"
+        {...props}  
+        />
+        
     )
 }
--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,31 +1,21 @@
 import ReactDOM from "react-dom";
-import { useNavigate } from "react-router-dom";
-
-
+import { Link } from "react-router-dom";
 
 import ilustrationImg from "../assets/images/illustration.svg";
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
 
 import { Button } from '../components/Button'
-import { useAuth } from "../hooks/useAuth";
+//import { useAuth } from "../hooks/useAuth";
 
 
 import '../styles/auth.scss';
 
-export function Home() {
+export function NewRoom() {
 
-    const history = useNavigate();
-    const { user, signInWithGoogle } = useAuth();
 
-    
+    //const { user } = useAuth();
 
-    async function handleCreateRoom () {
-            if(!user){
-                await signInWithGoogle()
-            }
-            history('/rooms/new')
-    }
 
     return (
         <div id="page-auth">
@@ -37,25 +27,20 @@ export function Home() {
             <main>
                 <div className="main-content">
                     <img src={logoImg} alt="Letmeask" />
-                    <button
-                    onClick={handleCreateRoom}
-                    className="create-room" >
-                        <img src={googleIconImg} alt="Logo do Google" />
-                        Crie sua sala com o Google
-                    </button>
-                    <div className="separator">
-                        ou entre em uma sala
-                    </div>
+                    <h2>Criar uma nova sala</h2>
                     <form>
                         <input 
 
                         type="text" 
-                        placeholder='Digite o código da sala'
+                        placeholder='Nome da sala'
                         />
                         <Button type='submit'>
-                            Entrar na sala
+                            Criar sala
                         </Button>
                     </form>
+                    <p>
+                        Quer entrar em uma sala existente? <Link to="/">clique aqui</Link>
+                    </p>
                 </div>
             </main>
         </div>
